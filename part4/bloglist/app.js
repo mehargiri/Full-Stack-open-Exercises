523dc46a497d4b6c6dbe92a73b66c150573d4375
Mehar Giri <84210815+mehargiri@users.noreply.github.com>
2024-09-10 20:11:43 -0400

    Fixed build:ui script of bloglist backend in part4
    Added code to app.js to include static bloglist frontend build on production mode

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,32 +1,36 @@
-import cors from "cors";
-import express from "express";
-import "express-async-errors";
-import mongoose from "mongoose";
-import blogRouter from "./controllers/blogs.js";
-import loginRouter from "./controllers/login.js";
-import testingRouter from "./controllers/testing.js";
-import userRouter from "./controllers/users.js";
-import { MONGO_URI } from "./utils/config.js";
-import { info } from "./utils/logger.js";
-import { errorHandler } from "./utils/middleware.js";
+import cors from 'cors';
+import express from 'express';
+import 'express-async-errors';
+import mongoose from 'mongoose';
+import blogRouter from './controllers/blogs.js';
+import loginRouter from './controllers/login.js';
+import testingRouter from './controllers/testing.js';
+import userRouter from './controllers/users.js';
+import { MONGO_URI } from './utils/config.js';
+import { info } from './utils/logger.js';
+import { errorHandler } from './utils/middleware.js';
 
 const app = express();
 
 const connectDB = async () => {
-	await mongoose.connect(MONGO_URI);
-	info("Connected to the database");
+  await mongoose.connect(MONGO_URI);
+  info('Connected to the database');
 };
 connectDB();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/users", userRouter);
-app.use("/api/login", loginRouter);
-app.use("/api/blogs", blogRouter);
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('dist'));
+}
 
-if (process.env.NODE_ENV === "test") {
-	app.use("/api/testing", testingRouter);
+app.use('/api/users', userRouter);
+app.use('/api/login', loginRouter);
+app.use('/api/blogs', blogRouter);
+
+if (process.env.NODE_ENV === 'test') {
+  app.use('/api/testing', testingRouter);
 }
 
 app.use(errorHandler);
